cabc6b671f2f40f29ea3273c5417d9170bbddb2c
SquidO22 <squidblade22@gmail.com>
2024-01-15 10:04:06 -0500

    Added "findHighestNum" function and tested vallidity

--- findTheHighestNum.js
+++ findTheHighestNum.js
@@ -1,5 +1,16 @@
-const findHighestNum = (list) => {
-    return [10]
+const findHighestNum = () => {
+    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let looper = 0;
+    let highestNum = 0;
+    while (looper <= list.length - 1){
+        if(list[looper] > highestNum){
+            highestNum = list[looper];
+            looper++;
+        } else{
+            looper++;
+        }
+    }
+    return highestNum;
 }
 
 module.exports = findHighestNum
